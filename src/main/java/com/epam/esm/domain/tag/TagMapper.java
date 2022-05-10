0eb39a0dcceb208722a4660f6876dbde837daf3b
Farrux <Farrux@gmail.com>
2022-05-10 09:44:50 +0500

    domain created

---
+++ src/main/java/com/epam/esm/domain/tag/TagMapper.java
@@ -0,0 +1,17 @@
+package com.epam.esm.domain.tag;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class TagMapper implements RowMapper<Tag> {
+    @Override
+    public Tag mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Tag tag = new Tag();
+        tag.setId(UUID.fromString(rs.getString("id")));
+        tag.setName(rs.getString("name"));
+        return tag;
+    }
+}
